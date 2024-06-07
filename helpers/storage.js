638229fa71970237701620c3bd9f4ec010f3cc8d
NikolaTosic-sudo <toleslaoffice@gmail.com>
2024-06-07 20:53:07 +0200

    Add utils for manipulating LocalStorage and setting cookies for authorization expiration

---
+++ helpers/storage.js
@@ -0,0 +1,23 @@
+export const getLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+export const setLocalStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value));
+};
+
+export function getCookie(name, line) {
+  const value = `; ${line}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+export function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
